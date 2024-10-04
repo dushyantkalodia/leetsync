54128d2ea3d7e6670a51cc2a8ed5db6c9d675df9
Dushyant Kalodia <86359276+dushyantkalodia@users.noreply.github.com>
2024-10-04 23:31:49 +0530

    Time: 40 ms (52.16%) | Memory: 16.7 MB (82.15%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -5,7 +5,9 @@ class Solution:
         nums.sort()
 
         def create_subset(i):
-            
+            if (subset[:]) in res:
+                return
+
             if i == len(nums):
                 res.append(subset[:])
                 return
@@ -14,8 +16,6 @@ class Solution:
             create_subset(i+1)
 
             subset.pop()
-            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
-                i += 1
             create_subset(i+1)
         
         create_subset(0)
