78f19758f92d19cc10e50f07395649193c46399a
Dushyant Kalodia <86359276+dushyantkalodia@users.noreply.github.com>
2024-11-23 22:46:19 +0530

    Time: 2845 ms (11.70%) | Memory: 29.1 MB (19.83%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.py
@@ -0,0 +1,27 @@
+class Solution:
+    def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
+        m = len(box)
+        n = len(box[0])
+        res = [['' for _ in range(m)] for _ in range(n)]
+
+        for i in range(n):
+            for j in range(m):
+                res[i][j] = box[j][i]
+        
+        for row in res:
+            row.reverse()
+
+        for j in range(m):
+            for i in range(n-1,-1,-1):
+                if res[i][j] == '.':
+                    stoneRow = -1
+                    for k in range(i-1,-1,-1):
+                        if res[k][j] == '*':
+                            break
+                        elif res[k][j] == '#':
+                            stoneRow = k
+                            break
+                    if stoneRow != -1:
+                        res[i][j] = '#'
+                        res[stoneRow][j] = '.'
+        return res
