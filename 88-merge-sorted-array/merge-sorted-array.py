4124d78d895114657822fb5430808c41de3149ea
Dushyant Kalodia <86359276+dushyantkalodia@users.noreply.github.com>
2025-02-07 14:45:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (80.99%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -5,14 +5,13 @@ class Solution:
         """
         last = m+n-1
         while n>0 and m>0:
-            if nums1[m-1]>nums2[n-1]:
+            if nums1[m-1] > nums2[n-1]:
                 nums1[last] = nums1[m-1]
                 m -= 1
             else:
                 nums1[last] = nums2[n-1]
                 n -= 1
-            last -= 1
+            last -=1
         while n>0:
             nums1[last] = nums2[n-1]
             n,last = n-1,last-1
-
